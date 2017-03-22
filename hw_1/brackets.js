43b7fb6e41af8b32e1c09b37fa4a64a6d01b2f66
mmf_kupl <ikuplevich@mail.ru>
2017-03-22 13:54:34 +0300

    update

--- hw_1/brackets.js
+++ hw_1/brackets.js
@@ -17,16 +17,15 @@ alert(str + '\n' + tmp);
 
 var tmp1 = 0;
 
-if(tmp[0] == '\u0029')
-	alert('Скобки написаны неправильно!');
-else{
-	for(var i = 0; i < tmp.length; i++){
+for(var i = 0; i < tmp.length; i++){
 		switch(tmp[i]){
-			case '\u0029' : tmp1 --; break;
 			case '\u0028' : tmp1 ++; break;
+			case '\u0029' : tmp1 --; break;
+		}
+		if(tmp1 < 0) {
+			alert('Скобки написаны неправильно!');
+			break;
 		}
 	}
 if(tmp1 == 0) alert('Скобки написаны правильно!');
-	else
-		alert('Скобки написаны неправильно!');
-}
+
