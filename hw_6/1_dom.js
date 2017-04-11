c4aef6790f157c8d0583b68b3ba8d1c6da10b918
mmf_kupl <ikuplevich@mail.ru>
2017-04-11 11:03:29 +0300

    hw_6

---
+++ hw_6/1_dom.js
@@ -0,0 +1,14 @@
+﻿var elm = 0, com = 0, txt = 0;
+
+var searchDOM = function(element){
+	console.log(element);
+	if(element.nodeType == 1) elm++;
+	else if(element.nodeType == 8) com++;
+	else if(element.nodeType == 3) txt++;
+
+	if(element.childNodes.length) for (var i = 0; i < element.childNodes.length; i++) searchDOM(element.childNodes[i]);
+}
+
+searchDOM(document);
+
+console.log("Кол-во тегов: " + elm + "\nКол-во комментариев: " + com + "\nКол-во текстовых узлов: " + txt);
---
+++ hw_6/3_X.js
@@ -0,0 +1,46 @@
+﻿var Name = ["Bob","Carlton","Gordon",
+"Percy","Conrad","Quincey",
+"Armand","Jamal","Andrew",
+"Matthew","Mark","Gerald","Ivan", 
+"Pavel", "George", "Vladimir", "Mary", 
+"Yan", "Roman"];
+
+var City = ["Minsk", "Mogilev", "Grogno", "Gomel", "Brest"
+, "Vitebsk"];
+
+var goods = ["car", "kolbosa", "stol", "stul", "telephone"];
+var goodspng = ['png1/car.png', 'png1/kolb.png', 'png1/stol.png', 'png1/stul.png', 'png1/tlp.png'];
+
+var SPAM = [];
+
+for(var i = 0; i < goods.length; i++){
+	SPAM[i] = {	
+		name : goods[i],
+		png : goodspng[i]
+	}
+}
+
+var tbl = document.createElement("table");
+document.body.appendChild(tbl);
+
+var intr = setInterval(function(){
+	var i = Math.floor(Math.random()*goodspng.length);
+	var j = Math.floor(Math.random()*Name.length);
+	var k = Math.floor(Math.random()*City.length);
+	var tr = document.createElement("tr");
+	tbl.appendChild(tr);
+	var td = document. createElement("td");
+	tr.appendChild(td);
+	tr.innerHTML = "<img height=\"50\" width=\"150\" src=\"" + goodspng[i] + "\"</img>" + "<p>" + Name[j] + " tolko shto kupil " + goods[i] + " iz goroda: " + City[k] + "</p>";
+	var td = document. createElement("td");
+	tr.appendChild(td);
+	var button = document.createElement('button');
+	button.setAttribute('type', 'button');
+	button.innerText = 'X';
+	tr.appendChild(button);
+	var fun = function(){
+		this.parentNode.remove();
+	}
+	button.addEventListener("click", fun);	
+},3000);
+	
